1bd329bfa709970b065601605f679300f2b728bf
Didier Viret <didier.viret@orif.ch>
2024-10-27 21:55:14 +0100

    Add localization and common language file

---
+++ rambert/common/Language/fr/common_lang.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * French translations for common module
+ *
+ * @author      Didier Viret
+ * @link        https://github.com/DidierViret
+ * @copyright   Copyright (c), Didier Viret
+ */
+
+return[
+// Application name
+'app_title'                      => 'Fichier des membres',
+
+// Page titles
+'page_prefix'                    => 'Rambert',
+
+// Buttons
+'btn_admin'                      => 'Administration',
+'btn_login'                      => 'Se connecter',
+'btn_logout'                     => 'Se déconnecter',
+'btn_change_my_password'         => 'Modifier mon mot de passe',
+'btn_back'                       => 'Retour',
+'btn_cancel'                     => 'Annuler',
+'btn_save'                       => 'Enregistrer',
+'btn_add'                        => 'Ajouter',
+'btn_new_m'                      => 'Nouveau',
+'btn_new_f'                      => 'Nouvelle',
+'btn_details'                    => 'Détails',
+'btn_edit'                       => 'Modifier',
+'btn_disable'                    => 'Désactiver',
+'btn_reactivate'                 => 'Réactiver',
+'btn_show_disabled'              => 'Afficher les éléments désactivés',
+'btn_delete'                     => 'Supprimer',
+'btn_hard_delete'                => 'Supprimer définitivement',
+'btn_search'                     => 'Rechercher',
+'btn_restore'                    => 'Restaurer',
+'btn_copy'                       => 'Dupliquer',
+
+// Other common texts
+'yes'                            => 'Oui',
+'no'                             => 'Non',
+'all_m'                          => 'Tous',
+'all_f'                          => 'Toutes',
+'reactivate'                     => 'Réactiver',
+'initialization'                 => 'Initialisation',
+'initialize'                     => 'Initialiser',
+'for'                            => 'Pour',
+'click_on_the_button'            => 'Cliquez sur le bouton',
+'above'                          => 'Ci-dessous',
+];
