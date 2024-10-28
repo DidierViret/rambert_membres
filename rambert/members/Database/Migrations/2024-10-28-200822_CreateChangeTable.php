52d0e2d06579682e2e5074a6ab7da19a92873677
Didier Viret <didier.viret@orif.ch>
2024-10-28 21:14:44 +0100

    Add migration file for change table

---
+++ rambert/members/Database/Migrations/2024-10-28-200822_CreateChangeTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Members\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateChangeTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'fk_change_author' => [
+                'type'       => 'INT',
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+            'fk_person_concerned' => [
+                'type'       => 'INT',
+                'unsigned'   => true,
+                'null'       => true,
+            ],
+            'fk_change_type' => [
+                'type'       => 'INT',
+                'unsigned'   => true,
+                'null'       => true,
+            ],
+            'value_old' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'value_new' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'date' => [
+                'type'    => 'TIMESTAMP',
+                'null'    => false,
+                'default' => 'CURRENT_TIMESTAMP',
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('fk_change_author', 'person', 'id', 'RESTRICT', 'RESTRICT');
+        $this->forge->addForeignKey('fk_person_concerned', 'person', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('fk_change_type', 'change_type', 'id', 'RESTRICT', 'RESTRICT');
+
+        $this->forge->createTable('change');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('change');
+    }
+}
