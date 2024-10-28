5902d92faebe51ae3cfb4c2d2bfafec8a5778458
Didier Viret <didier.viret@orif.ch>
2024-10-28 22:01:07 +0100

    Add migration file for contribution table

---
+++ rambert/members/Database/Migrations/2024-10-28-205839_CreateContributionTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Members\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateContributionTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'fk_person' => [
+                'type' => 'INT',
+                'unsigned' => true,
+            ],
+            'fk_function' => [
+                'type' => 'INT',
+                'unsigned' => true,
+            ],
+            'date_begin' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'date_end' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('fk_person', 'person', 'id', 'RESTRICT', 'RESTRICT');
+        $this->forge->addForeignKey('fk_function', 'function', 'id', 'RESTRICT', 'RESTRICT');
+
+        $this->forge->createTable('contribution');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('contribution');
+    }
+}
