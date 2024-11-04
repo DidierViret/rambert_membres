5b77871a5772df8b5487f6775fdc1e507757b5ab
Didier Viret <didier.viret@orif.ch>
2024-11-04 21:22:13 +0100

    Use a custom Bootstrap css file

--- rambert/common/Views/header.php
+++ rambert/common/Views/header.php
@@ -31,7 +31,8 @@
     <link rel="shortcut icon" type="image/png" href="<?= base_url("images/favicon.png"); ?>" />
 
     <!-- Bootstrap  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Rambert Bootstrap CSS personalized with https://bootstrap.build/app -->
+    <link rel="stylesheet" href="<?= base_url("css/rambert_bootstrap.min.css"); ?>" />
     <!-- Bootstrap icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <!-- Bootstrap javascript -->
