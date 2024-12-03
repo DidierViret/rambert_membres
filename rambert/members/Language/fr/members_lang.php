ad9d1327ee05be8f85753dd109e67ecab24f9c66
Didier Viret <didier.viret@orif.ch>
2024-12-03 22:08:19 +0100

    Add language file for Members module

---
+++ rambert/members/Language/fr/members_lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * French translations for members module
+ *
+ * @author      Club Rambert, Didier Viret
+ * @link        https://rambert.ch
+ * @copyright   Copyright (c), club Rambert
+ */
+
+return[
+// Pages titles
+'title_'                            => '',
+
+// Forms fields
+'field_'                            => '',
+
+// Lists columns
+'col_'                              => '',
+
+// Buttons
+'btn_'                              => '',
+
+// Error messages
+'msg_error_valid_email'             => 'Veuillez entrer une adresse e-mail valide',
+'msg_error_email_length'            => 'L\'adresse e-mail ne doit pas dépasser 150 caractères',
+
+// Error code messages
+'code_error_401'                    => '401 - Non autorisé',
+'code_error_403'                    => '403 - Accès refusé',
+
+// Other texts
+
+];
