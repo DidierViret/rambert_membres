b810f4329382db25e8c2e9e01b125e706e355b57
Didier Viret <didier.viret@orif.ch>
2024-12-05 21:36:39 +0100

    Finalize homes importation

--- rambert/members/Controllers/ImportData.php
+++ rambert/members/Controllers/ImportData.php
@@ -32,6 +32,28 @@ class ImportData extends BaseController
         // Load required models
         $this->homeModel = new HomeModel();
         $this->personModel = new PersonModel();
+
+        // Connection to the joomla database
+        $joomla = [
+            'hostname' => 'localhost',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'rambert_joomla',
+            'DBDriver' => 'MySQLi',
+            'DBPrefix' => '',
+            'pConnect' => false,
+            'DBDebug'  => true,
+            'charset'  => 'utf8',
+            'DBCollat' => 'utf8_general_ci',
+            'swapPre'  => '',
+            'encrypt'  => false,
+            'compress' => false,
+            'strictOn' => false,
+            'failover' => [],
+            'port'     => 3306,
+            ];
+    
+        $this->dbJoomla = \Config\Database::connect($joomla);
     }
 
     /**
@@ -39,30 +61,8 @@ class ImportData extends BaseController
      */
     public function import()
     {
-        // Connection to the joomla database
-        $joomla = [
-        'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'database' => 'rambert_joomla',
-        'DBDriver' => 'MySQLi',
-        'DBPrefix' => '',
-        'pConnect' => false,
-        'DBDebug'  => true,
-        'charset'  => 'utf8',
-        'DBCollat' => 'utf8_general_ci',
-        'swapPre'  => '',
-        'encrypt'  => false,
-        'compress' => false,
-        'strictOn' => false,
-        'failover' => [],
-        'port'     => 3306,
-        ];
-
-        $dbJoomla = \Config\Database::connect($joomla);
-
         // Get all informations from comprofiler and users tables
-        $query = $dbJoomla->query('SELECT *
+        $query = $this->dbJoomla->query('SELECT *
                                    FROM lyf7s_comprofiler
                                    INNER JOIN lyf7s_users ON lyf7s_comprofiler.user_id=lyf7s_users.id');
 
@@ -72,21 +72,55 @@ class ImportData extends BaseController
         
         // Insert each member in the new database, splitting informations in different tables
         foreach($cbMembers as $cbMember) {
-            $home['address_title'] = $cbMember['cb_titre_envois'];
-            $home['address_name'] = $cbMember['cb_destinataires_envois'];
-            $home['address_line_1'] = $cbMember['cb_adresse'];
-            $home['address_line_2'] = null;
-            $home['postal_code'] = $cbMember['cb_codepostal'];
-            $home['city'] = $cbMember['cb_localite'];
-            $home['nb_bulletins'] = $cbMember['cb_nb_bulletins'];
-            $home['comments'] = null;
+            $homeId = $this->importHome($cbMember);
+
             
-            $homeId = $this->homeModel->insert($home);
         }
 
         dd($this->homeModel->findAll());
 
         // TODO : Script qui fait un soft_delete des homes dont tous les membres sont désactivés
+    }
+
+    /**
+     * Get home informations and check if this home allready exists in DB
+     * If not, add the new home in DB, else return the id of existing home
+     * 
+     * @param $cbMember : The member informations as registered in Joomla DB
+     * @return : The id of newly created home or of the corresponding existing home
+     */
+    protected function importHome($cbMember): int {
+        $home['address_title'] = $cbMember['cb_titre_envois'];
+        $home['address_name'] = $cbMember['cb_destinataires_envois'];
+        $home['address_line_1'] = $cbMember['cb_adresse'];
+        $home['address_line_2'] = null;
+        $home['postal_code'] = $cbMember['cb_codepostal'];
+        $home['city'] = $cbMember['cb_localite'];
+        $home['nb_bulletins'] = $cbMember['cb_nb_bulletins'];
+        $home['comments'] = null;
+
+        $existingHome = $this->homeModel->where(['address_line_1' => $home['address_line_1'],
+                                                 'city' => $home['city'],
+                                                 'postal_code' => $home['postal_code']])->first();
+        
+        if (!empty($existingHome)) {
+            // Keep the most relevant informations between existing home and cbMember's home
+            if (strlen($home['address_title']) > strlen($existingHome['address_title'])) {
+                $existingHome['address_title'] = $home['address_title'];
+            }
+            if (strlen($home['address_name']) > strlen($existingHome['address_name'])) {
+                $existingHome['address_name'] = $home['address_name'];
+            }
+            if ($home['nb_bulletins'] > $existingHome['nb_bulletins']) {
+                $existingHome['nb_bulletins'] = $home['nb_bulletins'];
+            }
+
+            // Update the allready existing home and return its id
+            return $this->homeModel->save($existingHome);
+        } else {
+            // Insert the new home and return its id
+            return $this->homeModel->save($home);
+        }
     }
 }
 
